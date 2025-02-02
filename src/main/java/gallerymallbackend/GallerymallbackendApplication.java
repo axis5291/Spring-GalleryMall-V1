3f6f3003746df2dbe2bae24b0097a58091c80915
Axis <erlia@naver.com>
2025-02-02 18:41:49 +0900

    Test

--- src/main/java/gallerymallbackend/GallerymallbackendApplication.java
+++ src/main/java/gallerymallbackend/GallerymallbackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GallerymallbackendApplication {
+public class GallerymallbackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GallerymallbackendApplication.class, args);
